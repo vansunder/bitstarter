2d8947ea1077c5d86351bb8f6f2cf0cc29bb9f99
David van Sunder <david@vansunder.com>
2013-07-10 18:45:24 +0000

    correction2 web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync("index.html", "utf-8"));
+    var filecontents = fs.readFileSync("index.html", "utf-8")
+    response.send(filecontents);
 });
 
 var port = process.env.PORT || 5000;
